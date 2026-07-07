3b4c358fe4f6ac23b5075180c1eb7f55ea394e65
Meroj <00342364+meroj-diverta@users.noreply.github.com>
2026-07-07 11:09:22 +0900

    custom index.vue route file has been created to fetch data from my API and display it. RUntime config has been added to nuxt.config.ts to make configs accessible across the project app.vue has been modidifed, default composable removed and replaced with NuxtPage composable that I assume runs index.html automatically.

---
+++ app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div v-for="n in response.details.ext_1" :key="n.slag">
+            <img :src="n.ext_1.url" width="400">
+            <div>id: {{ n.ext_1.id }}</div>
+            <div>desc: {{ n.ext_1.desc }}</div>
+            <div>url_org: {{ n.ext_1.url_org }}</div>
+            <img :src="n.ext_2.url" width="400">
+            <div>id: {{ n.ext_2.id }}</div>
+            <div>desc: {{ n.ext_2.desc }}</div>
+            <div>url_org: {{ n.ext_2.url_org }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+const { data: response } = await useFetch(
+    `${config.public.apiBase}/rcms-api/2/services/3`,
+    {
+        credentials: 'include',
+    }
+);
+</script>
